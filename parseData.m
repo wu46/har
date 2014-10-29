aeb44400105ec62f3282a7d480489a4ee8eeba79
Jennifer Wu <jenniferwu46@gmail.com>
2014-10-28 17:39:58 -0700

    Changed class from strings to ints.

--- parseData.m
+++ parseData.m
@@ -1,16 +1,17 @@
 % CS229 | Project | Jennifer Wu |10/19/14
 
-function data = parseData
+function [data, classRange] = parseData
 % Read in data
 startLine = 1;
 nRows = 100;
 nColumns = 19;
+classRange = zeros(5,2);
 
-fname = 'dataset-har-PUC-Rio-ugulino.csv';
+fname = 'dataset_modified.csv';
 fid = fopen(fname, 'r');
-rawdata = textscan(fid, '%s%s%d%d%c%d%f%d%c%d%f%f%f%f%f%f%f%f%f%f%f%f%s',...
+rawdata = textscan(fid, '%s%s%d%d%c%d%f%d%c%d%f%f%f%f%f%f%f%f%f%f%f%f%d',...
     'Delimiter', ';',...
-    'Headerlines', 1); 
+    'Headerlines', 2); 
 fclose(fid);
 data.name = rawdata{1};
 data.gender = rawdata{2};
@@ -31,4 +32,10 @@ data.x4 = rawdata{20};
 data.y4 = rawdata{21};
 data.z4 = rawdata{22};
 data.class = rawdata{23};
+
+for iClass = [0:4]
+    ind = find(data.class == iClass);
+    classRange(iClass+1,1) = ind(1);
+    classRange(iClass+1,2) = ind(end);
+end
 end
